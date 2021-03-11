4f1a8119254b1dde75723412427ee1ce18001392
ssw1426 <80308398+ssw1426@users.noreply.github.com>
2021-03-12 00:10:25 +0900

    Update main1.py

--- main1.py
+++ main1.py
@@ -15,8 +15,8 @@ async def on_message(message):
         try:
             if message.author.guild_permissions.manage_messages:
                 await message.channel.send("@everyone")
-                embed = discord.Embed(title="서버 온 안내", description="voice 서버가 열렸습니다. 즐거운 RP생활 되세요", color=0x62c1cc)
-                embed.add_field(name="다이렉트 주소", value="cfx.re/join/84ol95", inline=False)
+                embed = discord.Embed(title="서버 온 안내", description="List 서버가 열렸습니다. 즐거운 RP 되세요", color=0x62c1cc)
+                embed.add_field(name="다이렉트 주소", value="connect cfx.re/join/84ol95", inline=False)
                 await message.channel.send(embed=embed)
                 await message.delete()
             else:
@@ -29,7 +29,7 @@ async def on_message(message):
         try:
             if message.author.guild_permissions.manage_messages:
                 await message.channel.send("@everyone")
-                embed = discord.Embed(title="서버 리붓 안내", description="voice 서버가 리붓되었습니다. 즐거운 RP생활 되세요", color=0xFFE400)
+                embed = discord.Embed(title="서버 리붓 안내", description="List 서버가 리붓되었습니다. 즐거운 RP 되세요", color=0xFFE400)
                 await message.channel.send(embed=embed)
                 await message.delete()
             else:
@@ -42,7 +42,7 @@ async def on_message(message):
             if message.author.guild_permissions.manage_messages:
                 await message.channel.send("@everyone")
                 embed = discord.Embed(title="서버 오프 안내", description="서버 문제 및 패치로 서버가 닫혔습니다", color=0xff0000)
-                embed.add_field(name="서버 온 되면 들어와주세요", value="저희 voice 서버는 패치 및 버그에 최선를 다합니다", inline=False)
+                embed.add_field(name="서버 온 되면 들어와주세요", value="저희 List 서버는 패치 및 버그 수정에 최선를 다합니다", inline=False)
                 await message.channel.send(embed=embed)
                 await message.delete()
             else:

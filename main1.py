648a84ee651f1f9d1c62c96b583ccd673654e0dd
ssw1426 <80308398+ssw1426@users.noreply.github.com>
2021-03-11 11:01:03 +0900

    Update main1.py

--- main1.py
+++ main1.py
@@ -18,8 +18,10 @@ async def on_message(message):
                 embed = discord.Embed(title="서버 온 안내", description="voice 서버가 열렸습니다. 즐거운 RP생활 되세요", color=0x62c1cc)
                 embed.add_field(name="다이렉트 주소", value="cfx.re/join/84ol95", inline=False)
                 await message.channel.send(embed=embed)
+                await message.delete()
             else:
                 await message.channel.send('``명령어 사용권한이 없습니다.``')
+                await message.delete()
         except:
             pass
 
@@ -29,8 +31,10 @@ async def on_message(message):
                 await message.channel.send("@everyone")
                 embed = discord.Embed(title="서버 리붓 안내", description="voice 서버가 리붓되었습니다. 즐거운 RP생활 되세요", color=0xFFE400)
                 await message.channel.send(embed=embed)
+                await message.delete()
             else:
                 await message.channel.send('``명령어 사용권한이 없습니다.``')
+                await message.delete()
         except:
             pass
     if message.content.startswith("!off"):
@@ -40,8 +44,10 @@ async def on_message(message):
                 embed = discord.Embed(title="서버 오프 안내", description="서버 문제 및 패치로 서버가 닫혔습니다", color=0xff0000)
                 embed.add_field(name="서버 온 되면 들어와주세요", value="저희 voice 서버는 패치 및 버그에 최선를 다합니다", inline=False)
                 await message.channel.send(embed=embed)
+                await message.delete()
             else:
                 await message.channel.send('``명령어 사용권한이 없습니다.``')
+                await message.delete()
         except:
             pass
 

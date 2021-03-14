d56e524753bca8484aac6554e8e3f3a5768a5fab
ssw1426 <80308398+ssw1426@users.noreply.github.com>
2021-03-14 22:57:25 +0900

    Update main1.py

--- main1.py
+++ main1.py
@@ -15,8 +15,8 @@ async def on_message(message):
         try:
             if message.author.guild_permissions.manage_messages:
                 await message.channel.send("@everyone")
-                embed = discord.Embed(title="[FAST Server System ON]", description="- 접속 후 서버 플레이에 지장이 있다면 DM 남겨주세요.", color=0x62c1cc)
-                embed.add_field(name="다이렉트 주소", value="connect cfx.re/join/m4ma39", inline=False)
+                embed = discord.Embed(title="[ FAST Server System ON ]", description="- 접속 후 서버 플레이에 지장이 있다면 DM 남겨주세요.", color=0x62c1cc)
+                embed.add_field(name="[ Direct Address ]", value="connect cfx.re/join/m4ma39", inline=False)
                 embed.add_field(name="[ KOREA FAST RP Server ]", value="- 24시간 ON 상태 입니다. 열심히 노력하는 FAST RP Server 가 되도록하겠습니다.", inline=False)
                 await message.channel.send(embed=embed)
                 await message.delete()
@@ -30,8 +30,8 @@ async def on_message(message):
         try:
             if message.author.guild_permissions.manage_messages:
                 await message.channel.send("@everyone")
-                embed = discord.Embed(title="[FAST Server System REBOOT]", description="-FAST RP 서버가 현재 리붓중입니다", color=0xFFE400)
-                embed.add_field(name="다이렉트 주소", value="connect cfx.re/join/m4ma39", inline=False)
+                embed = discord.Embed(title="[ FAST Server System REBOOT ]", description="-FAST RP 서버가 현재 리붓중입니다", color=0xFFE400)
+                embed.add_field(name="[ Direct Address ]", value="connect cfx.re/join/m4ma39", inline=False)
                 embed.add_field(name="[ KOREA FAST RP Server ]", value="- 24시간 ON 상태 입니다. 열심히 노력하는 FAST RP Server 가 되도록하겠습니다.", inline=False)
                 await message.channel.send(embed=embed)
                 await message.delete()
@@ -44,8 +44,8 @@ async def on_message(message):
         try:
             if message.author.guild_permissions.manage_messages:
                 await message.channel.send("@everyone")
-                embed = discord.Embed(title="[FAST Server System OFF]", description="- 현재 서버 문제 및 패치로 서버가 닫혔습니다. 오픈되면 들어와주세요", color=0xff0000)
-                embed.add_field(name="다이렉트 주소", value="connect cfx.re/join/m4ma39", inline=False)
+                embed = discord.Embed(title="[ FAST Server System OFF ]", description="- 현재 서버 문제 및 패치로 서버가 닫혔습니다. 오픈되면 들어와주세요", color=0xff0000)
+                embed.add_field(name="[ Direct Address ]", value="connect cfx.re/join/m4ma39", inline=False)
                 embed.add_field(name="[ KOREA FAST RP Server ]", value="- 24시간 ON 상태 입니다. 열심히 노력하는 FAST RP Server 가 되도록하겠습니다.", inline=False)
                 await message.channel.send(embed=embed)
                 await message.delete()

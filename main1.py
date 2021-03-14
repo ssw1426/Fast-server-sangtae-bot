8e7173d8eae59224ac528e26da59af9a652d36bd
ssw1426 <80308398+ssw1426@users.noreply.github.com>
2021-03-14 13:54:27 +0900

    Update main1.py

--- main1.py
+++ main1.py
@@ -19,6 +19,7 @@ async def on_message(message):
                 embed.add_field(name="다이렉트 주소", value="connect cfx.re/join/84ol95", inline=False)
                 await message.channel.send(embed=embed)
                 await message.delete()
+                await message.delete()
             else:
                 await message.channel.send('``명령어 사용권한이 없습니다.``')
                 await message.delete()
@@ -32,6 +33,7 @@ async def on_message(message):
                 embed = discord.Embed(title="서버 리붓 안내", description="List 서버가 리붓되었습니다. 즐거운 RP 되세요", color=0xFFE400)
                 await message.channel.send(embed=embed)
                 await message.delete()
+                await message.delete()
             else:
                 await message.channel.send('``명령어 사용권한이 없습니다.``')
                 await message.delete()
@@ -44,6 +46,7 @@ async def on_message(message):
                 embed = discord.Embed(title="서버 오프 안내", description="서버 문제 및 패치로 서버가 닫혔습니다", color=0xff0000)
                 embed.add_field(name="서버 온 되면 들어와주세요", value="저희 List 서버는 패치 및 버그 수정에 최선를 다합니다", inline=False)
                 await message.channel.send(embed=embed)
+                await message.delete()
                 await message.delete()
             else:
                 await message.channel.send('``명령어 사용권한이 없습니다.``')

5265ce0577954e6333134098139f75a7656d8ba6
ssw1426 <80308398+ssw1426@users.noreply.github.com>
2021-03-14 22:50:21 +0900

    Update main1.py

--- main1.py
+++ main1.py
@@ -15,27 +15,28 @@ async def on_message(message):
         try:
             if message.author.guild_permissions.manage_messages:
                 await message.channel.send("@everyone")
-                embed = discord.Embed(title="서버 온 안내", description="List 서버가 열렸습니다. 즐거운 RP 되세요", color=0x62c1cc)
-                embed.add_field(name="다이렉트 주소", value="connect cfx.re/join/84ol95", inline=False)
+                embed = discord.Embed(title="[FAST Server System ON", description="- 접속 후 서버 플레이에 지장이 있다면 DM 남겨주세요.", color=0x62c1cc)
+                embed.add_field(name="다이렉트 주소", value="connect cfx.re/join/m4ma39", inline=False)
+                embed.add_field(name="[ KOREA FAST RP Server ]", value="- 24시간 ON 상태 입니다. 열심히 노력하는 FAST RP Server 가 되도록하겠습니다.", inline=False)
                 await message.channel.send(embed=embed)
                 await message.delete()
-                await message.delete()
             else:
-                await message.channel.send('``명령어 사용권한이 없습니다.``')
+                await message.channel.send('```명령어 사용권한이 없습니다.```')
                 await message.delete()
         except:
             pass
 
-    if message.content.startswith("!r"):
+    if message.content.startswith("!re"):
         try:
             if message.author.guild_permissions.manage_messages:
                 await message.channel.send("@everyone")
-                embed = discord.Embed(title="서버 리붓 안내", description="List 서버가 리붓됩니다. 즐거운 RP 되세요", color=0xFFE400)
+                embed = discord.Embed(title="[FAST Server System REBOOT]", description="-FAST RP 서버가 현재 리붓중입니다", color=0xFFE400)
+                embed.add_field(name="다이렉트 주소", value="connect cfx.re/join/m4ma39", inline=False)
+                embed.add_field(name="[ KOREA FAST RP Server ]", value="- 24시간 ON 상태 입니다. 열심히 노력하는 FAST RP Server 가 되도록하겠습니다.", inline=False)
                 await message.channel.send(embed=embed)
                 await message.delete()
-                await message.delete()
             else:
-                await message.channel.send('``명령어 사용권한이 없습니다.``')
+                await message.channel.send('```명령어 사용권한이 없습니다.```')
                 await message.delete()
         except:
             pass
@@ -43,13 +44,13 @@ async def on_message(message):
         try:
             if message.author.guild_permissions.manage_messages:
                 await message.channel.send("@everyone")
-                embed = discord.Embed(title="서버 오프 안내", description="서버 문제 및 패치로 서버가 닫혔습니다", color=0xff0000)
-                embed.add_field(name="서버 온 되면 들어와주세요", value="저희 List 서버는 패치 및 버그 수정에 최선를 다합니다", inline=False)
+                embed = discord.Embed(title="[FAST Server System OFF]", description="- 현재 서버 문제 및 패치로 서버가 닫혔습니다. 오픈되면 들어와주세요", color=0xff0000)
+                embed.add_field(name="다이렉트 주소", value="connect cfx.re/join/m4ma39", inline=False)
+                embed.add_field(name="[ KOREA FAST RP Server ]", value="- 24시간 ON 상태 입니다. 열심히 노력하는 FAST RP Server 가 되도록하겠습니다.", inline=False)
                 await message.channel.send(embed=embed)
                 await message.delete()
-                await message.delete()
             else:
-                await message.channel.send('``명령어 사용권한이 없습니다.``')
+                await message.channel.send('```명령어 사용권한이 없습니다.```')
                 await message.delete()
         except:
             pass

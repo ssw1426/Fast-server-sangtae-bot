777fb68ffccf7b972d93dcd6c5cb15230621aea4
ssw1426 <80308398+ssw1426@users.noreply.github.com>
2021-03-15 17:16:49 +0900

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,4 +1,5 @@
 import discord
+import asyncio
 import os
 
 client = discord.Client()
@@ -52,6 +53,23 @@ async def on_message(message):
             else:
                 await message.channel.send('```명령어 사용권한이 없습니다.```')
                 await message.delete()
+        except:
+            pass
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!클린'):
+        try:
+            # 메시지 관리 권한 있을시 사용가능
+            if message.author.guild_permissions.manage_messages:
+                amount = message.content[4:]
+                await message.delete()
+                await message.channel.purge(limit=int(amount))
+                message = await message.channel.send(embed=discord.Embed(title='🧹' + str(amount) + '개의 메시지가 삭제되었습니다', colour=discord.Colour.green()))
+                await asyncio.sleep(2)
+                await message.delete()
+            else:
+                await message.channel.send('```명령어 사용권한이 없습니다.```')
         except:
             pass
 
